8b02681e9715207a4037baf1827d32c84d68846c
anya ugochukwu miracle <anyamiracle59@gmail.com>
2025-05-09 17:21:34 -0700

    Added Test File

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+import { strict } from "assert";
+
+describe("Sample Test Suite", () => {
+  it("should return true for 1 + 1 === 2", () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+
+  it("should return false for 1 + 1 === 3", () => {
+    assert.notStrictEqual(1 + 1, 3);
+  });
+});
